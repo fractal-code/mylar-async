bf35fbb56169c47ddc5c36237c99ca10ec3c1583
Yaser Jarrar <yaser@grout.io>
2014-10-20 13:54:51 +0400

    Bump version to 0.1.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Collection of functions for async logic.",
     name: "mylar:async",
-    version: "0.0.1",
+    version: "0.1.0",
     git: "https://github.com/gliesesoftware/mylar-async.git"
 });
 
